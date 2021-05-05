92b2eddb869dda6f65d9e10606f0e23f743ec179
epociask <empociask@dons.usfca.edu>
2021-05-05 04:35:19 -0700

    [first_iteration] sheesh

--- example/example.go
+++ example/example.go
@@ -11,8 +11,8 @@ func main() {
 	cfg := &stats.Config{
 		ConveyorCount:  1,
 		FlushTime:      200,
-		Url:            "https://graphite-us-central1.grafana.net/metrics",
-		ApiKey:         "22241:eyJrIjoiMWE2ODI4MTEyOGQ0YWE2NDNlNjIwOWFiMzk2YzYyMmUwMjU2ZWEwOSIsIm4iOiJWb2xhdHJhZGUiLCJpZCI6NDQ4NTc0fQ==",
+		Url:            "SHEESH",
+		ApiKey:         "KEY",
 		Env:            "dev_env",
 		Service:        "fake_test_service",
 		RequestTimeout: 3,
@@ -27,6 +27,6 @@ func main() {
 	for {
 		statz.Increment("bleh", 1.0)
 		time.Sleep(time.Duration(100) * time.Millisecond)
-		statz.Increment("fudge", 1.0)
+		statz.Increment("bleh", 1.0)
 	}
 }
